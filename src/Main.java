7c5b518ee6fc2df637b18d831bad5f403272f0fd
weerr12 <jokerwwe0542@gmail.com>
2023-11-22 20:42:08 +0700

    lab01 nonnegative number

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,42 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// อ้างอิงค์แลป : https://www.youtube.com/watch?v=86B5U15c88w
+//            https://www.youtube.com/watch?v=RJZIJdYMsYg&t=26839s
+//            https://www.youtube.com/watch?v=AOZNmiUhduM
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter nonnegative number: ");
+        int nonnegative = sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        if(nonnegative>0){
+            // สร้าง array มารับ
+            int[] numbers = new int[nonnegative];
+            for (int i = 0; i < nonnegative; i++) {
+                System.out.print("Enter number" + (i + 1) + " : ");
+                numbers[i] = sc.nextInt();
+            }
+            //เรียกใช้ methods swap
+            swap(numbers);
+            System.out.print("Sorted numbers: ");
+            for (int num : numbers) {
+                System.out.print(num + " ");
+            }
+        }else{
+            System.out.println("Only nonnegative number !!!");
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+    public static void swap(int[] arr) {
+        int n = arr.length;
+        int temp ;
+        for(int i = 0; i< n; i++){
+            for(int j=i+1; j < n; j++){
+                if(arr[j] < arr[i]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
     }
 }
